1f1289c9391963e610595df171f2b17fcd4534c6
Minkxx <monsurcodes@gmail.com>
2026-07-17 13:14:17 +0530

    feat: update host permissions and minimum Firefox version in configuration

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -1,21 +1,20 @@
 import { defineConfig } from "wxt";
+import { strategies } from "./strategies";
+import { extractMatches } from "./utils/extractor";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
 	manifest: {
 		name: "Anime RPC",
-		host_permissions: ["*://anikototv.to/*", "*://vidtube.site/*"],
+		host_permissions: extractMatches(strategies),
 		browser_specific_settings: {
 			gecko: {
 				id: "anime-rpc@minkxx.dev",
-				strict_min_version: "109.0",
+				strict_min_version: "142.0",
 				data_collection_permissions: {
 					required: ["none"],
 				},
 			},
 		},
-	},
-	suppressWarnings: {
-		firefoxDataCollection: true,
 	},
 });
