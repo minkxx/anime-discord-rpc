3f8330d4a6ebb8144c1635fd7e5949d56675349e
Minkxx <monsurcodes@gmail.com>
2026-07-18 20:59:52 +0530

    feat: add animepaheStrategy to strategies

--- extension/strategies/index.ts
+++ extension/strategies/index.ts
@@ -1,4 +1,5 @@
 import { anikotoStrategy } from "./anikoto";
+import { animepaheStrategy } from "./animepahe";
 import { aniwaveStrategy } from "./aniwave";
 
-export const strategies = [anikotoStrategy, aniwaveStrategy];
+export const strategies = [anikotoStrategy, aniwaveStrategy, animepaheStrategy];
