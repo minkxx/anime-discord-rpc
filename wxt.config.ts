f87ec4401b134e6585eb91ca5ef3bf069d2e4262
Minkxx <monsurcodes@gmail.com>
2026-09-13 16:15:12 +0530

    feat: update manifest configuration to support dynamic browser-specific settings

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,11 +3,14 @@ import { defineConfig } from "wxt";
 import { strategies } from "./strategies";
 import { extractMatches } from "./utils/extractor";
 
-// See https://wxt.dev/api/config.html
 export default defineConfig({
-	manifest: {
+	manifest: ({ browser }) => ({
 		name: "Anime RPC",
-		key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAu+VrTvAa1lImCgCA/7mHz/Uvrp5WojKj907yEZbFVzHI8opZitOZe2UsO0kJJg2vpz6SiGJqcswWheooujJiVWI8h2QVCwHnXXHRnrKfxTv9sLmTH8w3wyRwgibUPE+Yyc2GChRUjWJqH30FxrSxH47tbvnghJEZaqcmyAerPMwOSzOyGs6Qj5AZzHtMsCH6UHCf0wM+e1kLuqvXgm93Qsg7jOGlaK3zq84vqt2E1dzxwnv1DFyGVnA3YZYD3b8ShuGIeKHF+f3VmNGYoBaXEg5XWcFfIMcw64Igu2m0LMGBm7ix6bRLfZsQk1mPaZYA+XVb+Td3+4KOana94P1vtQIDAQAB",
+		...(browser === "chrome"
+			? {
+					key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAu+VrTvAa1lImCgCA/7mHz/Uvrp5WojKj907yEZbFVzHI8opZitOZe2UsO0kJJg2vpz6SiGJqcswWheooujJiVWI8h2QVCwHnXXHRnrKfxTv9sLmTH8w3wyRwgibUPE+Yyc2GChRUjWJqH30FxrSxH47tbvnghJEZaqcmyAerPMwOSzOyGs6Qj5AZzHtMsCH6UHCf0wM+e1kLuqvXgm93Qsg7jOGlaK3zq84vqt2E1dzxwnv1DFyGVnA3YZYD3b8ShuGIeKHF+f3VmNGYoBaXEg5XWcFfIMcw64Igu2m0LMGBm7ix6bRLfZsQk1mPaZYA+XVb+Td3+4KOana94P1vtQIDAQAB",
+				}
+			: {}),
 		permissions: ["storage", "identity"],
 		host_permissions: [
 			...extractMatches(strategies),
@@ -23,7 +26,7 @@ export default defineConfig({
 				},
 			},
 		},
-	},
+	}),
 	vite: () => ({
 		plugins: [tailwindcss()],
 		server: {
